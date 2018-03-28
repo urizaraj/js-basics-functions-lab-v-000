baa8deef89ee7ed7126af11429d852a50d5dd406
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 19:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function distanceFromHqInBlocks(block) {
 function distanceFromHqInFeet(block) {
   return distanceFromHqInBlocks(block) * 264
 }
+
+function distanceTravelledInFeet(start, end) {
+  
+}
