0cfdc91c8114b8a5789cf9a6aa703549f26d356c
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 19:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(42 - block)
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264
+}
